6dc154d525215834cd884817c09ad8586c09d0fd
Shianne Lee <shiannelee@live.ca>
2017-11-09 04:09:28 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,7 @@ var chocolateBars = [
 
 function addElementToBeginningOfArray(array, element){
   return [element, ...array]
-  
+
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
   array = [element, ...array]
