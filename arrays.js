0bdb3ffd99ffd0646b9c993e6947823563f8918b
Shianne Lee <shiannelee@live.ca>
2017-11-09 04:01:44 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,36 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"
+]
+
+function addElementToBeginningOfArray(arr, element){
+  return [element, ...arr]
+}
+function destructivelyAddElementToBeginningOfArray(arr, element){
+  arr = [element, ...arr]
+  return arr
+}
+function addElementToEndOfArray(){
+  return [...arr, element]
+}
+function destructivelyAddElementToEndOfArray(){
+  arr = [...arr, element]
+  return arr
+}
+function accessElementInArray(arr, index){
+  return arr[index]
+}
+function destructivelyRemoveElementFromBeginningOfArray(arr){
+  return arr.shift()
+}
+function removeElementFromBeginningOfArray(arr){
+  return arr.slice(1)
+}
+function destructivelyRemoveElementFromEndOfArray(arr){
+  return arr.pop()
+}
+function removeElementFromEndOfArray(arr){
+  return arr.slice(0, arr.length - 1)
+}
